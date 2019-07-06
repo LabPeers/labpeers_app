57cb0c8ce61f6984d8769e8eab6a07f3e7b7bf1b
Nikola Egetenmeyer <egetenmeyer@Nikolas-MacBook-Pro.local>
2019-07-06 15:05:55 -0500

    Demo

--- hello/models.py
+++ hello/models.py
@@ -3,12 +3,14 @@ from django.forms import ModelForm
 
 # Create your models here.
 class Greeting(models.Model):
-    when = models.DateTimeField("date created", auto_now_add=True)
+     when = models.DateTimeField("date created", auto_now_add=True)
 
-class Input(models.Model):
-    r = models.FloatField()
-
-class InputForm(ModelForm):
-    class Meta:
-        model = Input
-        fields = "__all__"
+# =============================================================================
+# class Input(models.Model):
+#     r = models.FloatField()
+# 
+# class InputForm(ModelForm):
+#     class Meta:
+#         model = Input
+#         fields = "__all__"
+# =============================================================================
--- hello/views.py
+++ hello/views.py
@@ -1,39 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-#from django.shortcuts import render_to_response
-from django.template import RequestContext
-from .models import InputForm
-from .compute import compute
-
-import requests
-
-from .models import Greeting
+# =============================================================================
+# from django.shortcuts import render
+# from django.http import HttpResponse
+# 
+# #from django.shortcuts import render_to_response
+# from django.template import RequestContext
+# from .models import InputForm
+# from .compute import compute
+# 
+# import requests
+# 
+# from .models import Greeting
+# =============================================================================
 
 # Create your views here.
 #def index(request):
     # return HttpResponse('Hello from Python!')
  #   return render(request, "index.html")
+
+from django.shortcuts import render 
+from django.http import HttpResponse
+
+from .models import Greeting 
  
- 
+
+from bokeh.plotting import figure
+from bokeh.resources import CDN
+from bokeh.embed import components
+
+
+
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    print('Hello Niki World')
-    #return HttpResponse('<pre>' + r.text + '</pre>' + 'Hello Niki World')
+    plot = figure()
+    plot.circle([1,2], [3,4])
 
-    if request.method == 'POST':
-        form = InputForm(request.POST)
-        if form.is_valid():
-            form = form.save(commit=False)
-            return present_output(form)
-    else:
-        form = InputForm()
+    script, div = components(plot, CDN)
 
-    context = {'form': form}
-
-    return render(request,'hw1.html',context)
-
+    return render(request, "index.html", {"the_script": script, "the_div": div})
 
 
 
@@ -47,7 +49,72 @@ def db(request):
     return render(request, "db.html", {"greetings": greetings})
 
 
-def present_output(form):
-    r = form.r
-    s = compute(r)
-    return HttpResponse('Hello, World again! sin(%s)=%s' % (r, s))
+
+
+
+# =============================================================================
+# from bokeh.plotting import figure
+# from bokeh.io import output_file, show
+# 
+# def index(request):
+# 
+#     fg=figure(x_axis_label="x_axis", y_axis_label="y_axis")
+# 
+#     x=[1,2,3,4]
+#     y=[1,2,3,4]
+# 
+#     fg.circle(x,y)
+# 
+#     output_file('test_plot.html')
+#     return show(fg)
+# =============================================================================
+ 
+# =============================================================================
+# import matplotlib
+# matplotlib.use("Agg")
+# import matplotlib.pyplot as plt
+# 
+# from io import BytesIO
+# import base64
+#  
+#  
+# def myplot(request):
+#     buf = BytesIO()
+#     plt.savefig(buf, format='png', dpi=300)
+#     image_base64 = base64.b64encode(buf.getvalue()).decode('utf-8').replace('\n', '')
+#     buf.close()
+#     return render(request,'hw1.html',context)
+# =============================================================================
+
+
+# =============================================================================
+# def index(request):
+#     r = requests.get('http://httpbin.org/status/418')
+#     print(r.text)
+#     print('Hello Niki World')
+#     #return HttpResponse('<pre>' + r.text + '</pre>' + 'Hello Niki World')
+# 
+#     if request.method == 'POST':
+#         form = InputForm(request.POST)
+#         if form.is_valid():
+#             form = form.save(commit=False)
+#             return present_output(form)
+#     else:
+#         form = InputForm()
+# 
+#     context = {'form': form}
+# 
+#     return render(request,'hw1.html',context)
+# 
+# =============================================================================
+
+
+
+
+# =============================================================================
+# def present_output(form):
+#     r = form.r
+#     s = compute(r)
+#     return HttpResponse('Hello, World again! sin(%s)=%s' % (r, s))
+# 
+# =============================================================================
