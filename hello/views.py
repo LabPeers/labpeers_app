fb1139d8d5a3dd7085e4a5edeaa7a6727e80fa0b
Nikola Egetenmeyer <egetenmeyer@Nikolas-MacBook-Pro.local>
2019-09-17 11:43:45 -0500

    Demo

--- hello/views.py
+++ hello/views.py
@@ -79,7 +79,7 @@ class HomeView(TemplateView):
             source = ColumnDataSource(df)
             plot = figure(plot_width=400, plot_height=400, title=mytitle)
 
-            color_mapper = LinearColorMapper(palette = Viridis256, low = min(df['myBubble']), high = max(df['myBubble']))
+            color_mapper = LinearColorMapper(palette = Viridis256, low = min(df('myBubble')), high = max(df('myBubble')))
             #color_mapper = LinearColorMapper(palette = Viridis256, low = 1, high = 10)
             color_bar = ColorBar(color_mapper = color_mapper,
                                  location = (0, 0),
