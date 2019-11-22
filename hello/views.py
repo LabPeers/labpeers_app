035bd6ca1da8e11bcc631370e5833684dde4b4c6
Nikola Egetenmeyer <egetenmeyer@Nikolas-MacBook-Pro.local>
2019-11-22 13:02:38 -0600

    Demo

--- hello/views.py
+++ hello/views.py
@@ -112,7 +112,8 @@ class HomeView(TemplateView):
         table = DataTable(source=source1, columns=columns, width=400, height=400, editable=True)
         script2, div2 = components(table)
         
-        return render(request, self.template_name, {"users": users, "the_script": script, "the_div": div, "form": form, "the_script2":script2, "the_div2":div2})
+        return render(request, self.template_name, {"users": users, "the_script": script, "the_div": div, 
+                                                    "form": form, "the_script2":script2, "the_div2":div2})
     
     
     def post(self,request):
@@ -181,11 +182,10 @@ class HomeView(TemplateView):
             ]
             table = DataTable(source=source, columns=columns, width=400, height=400, editable=True)
 
+            script2, div2 = components(table)
             
-            script, div_dict = components({"plot": plot, "table": table}, CDN)
-            
-            
-        return render(request, self.template_name, {"the_script": script, "the_div": div_dict, "form": form, "table": table})
+        return render(request, self.template_name, {"the_script": script, "the_div": div, 
+                                                    "form": form, "the_script2":script2, "the_div2":div2})
         #return render(request, self.template_name, {"form": form})
 
             #print form.cleaned_data['my_form_field_name']
