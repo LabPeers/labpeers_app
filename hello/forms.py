ea883b8c8b29b626ee27afd628805c3d6bbf956f
Nikola Egetenmeyer <egetenmeyer@Nikolas-MacBook-Pro.local>
2019-11-24 22:33:57 -0600

    Demo

--- hello/forms.py
+++ hello/forms.py
@@ -17,11 +17,11 @@ class HomeForm(forms.ModelForm):
     graph_ylabel = forms.CharField(widget=forms.TextInput())
     myX = forms.CharField(widget=forms.TextInput())
     myY = forms.CharField(widget=forms.TextInput())
-#    myRadius= forms.CharField(widget=forms.TextInput())
+    myRadius= forms.CharField(widget=forms.TextInput())
     
 
     class Meta:
         model = Graph_Data
         #fields = ('post','myX',)
 #        fields = "__all__"
-        fields = ('graph_title','myX','myY','myRadius',)
+        fields = ('graph_title','graph_xlabel','graph_ylabel','myX','myY','myRadius',)
