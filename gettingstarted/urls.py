ebbfbaf57b64783a15beae76a531039cfdf5ffb0
Nikola Egetenmeyer <egetenmeyer@Nikolas-MacBook-Pro.local>
2019-11-26 10:58:41 -0600

    Demo

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -20,8 +20,8 @@ from hello.views import Register
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path("accounts/", include('accounts.urls')),
+    path("accounts/", include('django.contrib.auth.urls')),
     path("", HomeReal.as_view(), name='home'),
     path("bubblechart/", HomeView.as_view(), name='bubblechart'),
     path("register/", Register.as_view(), name='register'),
