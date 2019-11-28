13eded38ce222444a6fd7c52b29048fa48434a8a
Nikola Egetenmeyer <egetenmeyer@Nikolas-MacBook-Pro.local>
2019-11-28 13:13:51 -0600

    Demo

--- accounts/views.py
+++ accounts/views.py
@@ -2,10 +2,26 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth import login, logout, authenticate
 
 
-class SignUp(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    #success_url = '/accounts/login/'
-    template_name = 'signup.html'
+# =============================================================================
+# class SignUp(generic.CreateView):
+#     form_class = UserCreationForm
+#     success_url = reverse_lazy('login')
+#     #success_url = '/accounts/login/'
+#     template_name = 'signup.html'
+# =============================================================================
+
+
+def SignUp(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+    
+    
+    
+    form = UserCreationForm
+    return render(request, 'signup.html', {"form":form})
