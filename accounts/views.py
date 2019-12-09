38569a4280ee68a5a2ff3e99cd3ca2d1d08fe4d7
Nikola Egetenmeyer <egetenmeyer@Nikolas-MacBook-Pro.local>
2019-12-09 12:16:46 -0600

    Demo

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth import login, logout, authenticate
-from django.shortcuts import render, redirect 
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
 # =============================================================================
