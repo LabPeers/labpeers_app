206bfa29b90fd52b0167e818ec52207da0de90ac
Nikola Egetenmeyer <egetenmeyer@Nikolas-MacBook-Pro.local>
2020-01-09 10:10:50 -0600

    Upload Profile picture

--- bubble/views.py
+++ bubble/views.py
@@ -108,7 +108,7 @@ class Profile(TemplateView):
     
     def get(self, request):
         
-        p_form = UserProfileForm(instance=request.user.userprofile)
+        p_form = UserProfileForm(instance=request.user.UserProfile)
         args = {'user': request.user, 'p_form': p_form}
         
         return render(request, self.template_name, args)
