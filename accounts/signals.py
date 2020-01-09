c10843487b3bdf3c6152441ddaf0bff61c4419df
Nikola Egetenmeyer <egetenmeyer@Nikolas-MacBook-Pro.local>
2020-01-09 11:13:13 -0600

    Upload Profile picture

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jan  9 09:06:04 2020
+Created on Thu Jan  9 11:11:12 2020
 
 @author: egetenmeyer
 """
-from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import UserProfile
 
-
+    
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
@@ -19,5 +19,4 @@ def create_profile(sender, instance, created, **kwargs):
         
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
-
+    instance.userprofile.save()
