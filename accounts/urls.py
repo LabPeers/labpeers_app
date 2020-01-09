f97908fcbfb8a466cc19eb551719bf4ac423ee2f
Nikola Egetenmeyer <egetenmeyer@Nikolas-MacBook-Pro.local>
2020-01-09 14:00:44 -0600

    Upload Profile picture

--- accounts/urls.py
+++ accounts/urls.py
@@ -25,6 +25,11 @@ urlpatterns = [
 # =============================================================================
 ]
 
+urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
+
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)   
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
