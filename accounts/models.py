0e250f11e05576d3c52b086675fad918899d948b
Nikola Egetenmeyer <egetenmeyer@Nikolas-MacBook-Pro.local>
2020-01-08 22:28:17 -0600

    Upload Profile picture

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.conf import settings
+
 
 # Create your models here.
 
 class UserProfile(models.Model):
-    #user=models.OneToOneField(User,on_delete=models.CASCADE)
-    user = models.ForeignKey(User, unique=True, on_delete=models.CASCADE)
-    image=models.ImageField(upload_to='profile_image', blank=True, null=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    #user = models.ForeignKey(User, unique=True, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics', blank=True, null=True)
+    
+    def __str__(self):
+        return f'{self.user.username} UserProfile'
     
