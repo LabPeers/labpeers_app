5dbbb5f73ac1a48e363aba507de7c111df3f6177
Nikola Egetenmeyer <egetenmeyer@Nikolas-MacBook-Pro.local>
2020-01-10 13:01:30 -0600

    Upload Profile picture

--- gettingstarted/settings_development.py
+++ gettingstarted/settings_development.py
@@ -118,21 +118,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-)
+# Extra places for collectstatic to find static files.
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'live-static','static-root')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "live-static", "media-root")
+MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media-root")
 
 django_heroku.settings(locals())
 
