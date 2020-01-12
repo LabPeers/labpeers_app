25dbba1eddf7cd744f4d7495fbcb209f5ce04f0b
Nikola Egetenmeyer <egetenmeyer@Nikolas-MacBook-Pro.local>
2020-01-12 06:57:50 -0600

    Upload Profile picture

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "bubble",
     "accounts.apps.AccountsConfig",
+    "storages",
 ]
 
 MIDDLEWARE = [    
@@ -143,6 +144,19 @@ print(MEDIA_ROOT)
 MEDIAFILES_DIRS = (MEDIA_ROOT)
 
 django_heroku.settings(locals())
+
+
+#AWS Settings:
+
+AWS_QUERYSTRING_AUTH = False
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['labpeersbubble']
+MEDIA_URL = 'http://%s.s3.amazonaws.com/labpeers/' % AWS_STORAGE_BUCKET_NAME
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+
+
+
 
 #LOGIN_URL = '/login/'
 #LOGIN_REDIRECT_URL = '/bubblechart/'
