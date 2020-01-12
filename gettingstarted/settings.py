86afa1a9470862e45024cb6af6834a06b660e91a
Nikola Egetenmeyer <egetenmeyer@Nikolas-MacBook-Pro.local>
2020-01-12 09:40:43 -0600

    Upload Profile picture

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -153,7 +153,7 @@ AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
 MEDIA_URL = 'http://%s.s3.us-east-2.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_REGION = 'us-east-2'
 AWS_S3_HOST = 's3.%s.amazonaws.com' % AWS_REGION
 
