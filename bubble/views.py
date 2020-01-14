e8360371a5abeb2e07d087a55f03bc3e73d85fb9
Nikola Egetenmeyer <egetenmeyer@Nikolas-MacBook-Pro.local>
2020-01-14 13:49:54 -0600

    Plot images

--- bubble/views.py
+++ bubble/views.py
@@ -282,7 +282,7 @@ class HomeView(TemplateView):
                         plot = figure(plot_width=600, plot_height=600, title=mytitle, 
                                           x_axis_label=myXlabel, y_axis_label=myYlabel)
                         
-                        filename=myplotname + ".png"
+                        filename="plots/" + myplotname + ".png"
                         print(filename)
                         newplot=export_png(plot, filename)
                         plotimage= Gallery_Plots(plotname=myplotname, myplots=newplot)
