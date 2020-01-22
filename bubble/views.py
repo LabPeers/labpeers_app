07cd94d10a71d2b6be1d808d5463668b6cf5c288
Nikola Egetenmeyer <egetenmeyer@Nikolas-MacBook-Pro.local>
2020-01-22 13:51:13 -0600

    Range

--- bubble/views.py
+++ bubble/views.py
@@ -87,32 +87,27 @@ def bubbleplot(mytitle, myXlabel, myYlabel,myXlist, myYlist,myRlist):
 #    plot = figure(plot_width=600, plot_height=600, title=mytitle, 
 #                  x_axis_label=myXlabel, y_axis_label=myYlabel)
     
-    x1=min(myXlist)
-    print(x1)
-    index_x1=np.where(myXlist == x1)
-    print(index_x1)
-    index_x1=index_x1[0]
+    
+    
+    index_x1=myXlist.argmin()
     print(index_x1)
     myr1=myRlist[index_x1]
     print(myr1)
     myxmin=x1 - 0.7 * myr1
     print(myxmin)
     
-    x2=max(myXlist)
-    index_x2=np.where(myXlist == x2)
-    index_x2=index_x2[0]
+
+    index_x2=myXlist.argmax()
     myr2=myRlist[index_x2]
     myxmax=x2 + 0.7 * myr2
     
-    y1=min(myYlist)
-    index_y1=np.where(myYlist == y1)
-    index_y1=index_y1[0]
+
+    index_y1=myYlist.argmin()
     myr3=myRlist[index_y1]
     myymin=x1 - 0.7 * myr3
     
-    y2=max(myYlist)
-    index_y2=np.where(myYlist == y2)
-    index_y2=index_y2[0]
+ 
+    index_y2=myYlist.argmax()
     myr4=myRlist[index_y2]
     myymax=y2 + 0.7 * myr4
     
