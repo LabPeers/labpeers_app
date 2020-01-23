3c6db2640ca779773370c340bae2c333c55ed6a8
Nikola Egetenmeyer <egetenmeyer@Nikolas-MacBook-Pro.local>
2020-01-23 13:36:33 -0600

    Table

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     "bubble",
     "accounts.apps.AccountsConfig",
     "storages",
-    "django-tables2",
 ]
 
 MIDDLEWARE = [    
@@ -75,7 +74,6 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "django.template.context_processors.request",
             ]
         },
     }
