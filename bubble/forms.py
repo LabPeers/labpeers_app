cf22fb76f395f8983a731caf6bfff6cc38bb1475
Nikola Egetenmeyer <egetenmeyer@Nikolas-MacBook-Pro.local>
2020-01-27 10:14:38 -0600

    Table

--- bubble/forms.py
+++ bubble/forms.py
@@ -17,8 +17,8 @@ class HomeForm(forms.ModelForm):
     graph_ylabel = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'e.g. y-axis'}))
     graph_description = forms.CharField(required=False, widget=forms.Textarea(attrs={'placeholder':'e.g. This graph is very important!'}))
     myX = forms.CharField(widget=forms.TextInput(attrs={'id':'x-values','placeholder':'e.g. 1,2,3'}))
-    myY = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'e.g. 1,5,9'}))
-    myRadius= forms.CharField(widget=forms.TextInput(attrs={'placeholder':'e.g. 5,10,30'}))
+    myY = forms.CharField(widget=forms.TextInput(attrs={'id':'y-values','placeholder':'e.g. 1,5,9'}))
+    myRadius= forms.CharField(widget=forms.TextInput(attrs={'id':'Bubble-size','placeholder':'e.g. 5,10,30'}))
     myScale=forms.FloatField(widget=forms.NumberInput(attrs={'placeholder':'e.g. 1.2'}))
    # slug=forms.SlugField(widget=forms.TextInput())
     
