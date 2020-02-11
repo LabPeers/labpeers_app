745d29ae7ab05fa9b11091c6ec4c27e1ff25ca73
Nikola Egetenmeyer <egetenmeyer@Nikolas-MacBook-Pro.local>
2020-02-11 10:28:12 -0600

    Profile pic folder

--- accounts/models.py
+++ accounts/models.py
@@ -5,13 +5,18 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+def upload_pic(instance, filename):
+        return 'profile_pics/%s/%s' % (instance.user.user.username, filename)
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     #user = models.ForeignKey(User, unique=True, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='{user.username}/profile_pics', blank=True, null=True)
+    #image = models.ImageField(default='default.jpg', upload_to='{user.username}/profile_pics', blank=True, null=True)
+    image = models.ImageField(default='default.jpg', upload_to=upload_pic, blank=True, null=True)
     
     def __str__(self):
         return f'{self.user.username} UserProfile'
+
     
 #    def save(self):
 #        super().save()
