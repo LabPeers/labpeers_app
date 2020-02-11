22cc44151b8b3a2ee2755d31e104471d49bccbf1
Nikola Egetenmeyer <egetenmeyer@Nikolas-MacBook-Pro.local>
2020-02-11 10:37:57 -0600

    Profile pic folder

--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 def upload_pic(instance, filename):
-        return 'profile_pics/%s/%s' % (instance.user.user.username, filename)
+        return 'profile_pics/%s/%s' % (instance.user.username, filename)
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
