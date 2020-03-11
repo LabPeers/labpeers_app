e6bc1c897378655f42da02ef4945abb0d2c304ec
Nikola Egetenmeyer <egetenmeyer@Nikolas-MacBook-Pro.local>
2020-03-10 21:51:01 -0600

    ID

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = "gettingstarted.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR,'bubble/templates')],
+        "DIRS": [os.path.join(BASE_DIR,'mybubble/templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
